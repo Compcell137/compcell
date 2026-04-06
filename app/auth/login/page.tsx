13af31185616e92d7ec218525dddbfe4c8b19698
compcell <perucompcell@gmail.com>
2026-04-06 10:21:14 -0500

    Actualización general de diseño y archivos

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -233,7 +233,7 @@ export default function LoginPage() {
               </div>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 md:gap-2">
+            <div className="grid grid-cols-1 gap-2">
               <button
                 type="button"
                 onClick={handleGoogleLogin}
